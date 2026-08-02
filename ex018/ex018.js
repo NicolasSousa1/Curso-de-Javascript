260cd400de4ffc19363ee6aeb536672c61a3d210
Nicolas de Sousa <dsousa.nicolas@gmail.com>
2026-08-02 20:29:30 -0300

    ex 018

---
+++ ex018/ex018.js
@@ -0,0 +1,10 @@
+function analisar() {
+
+    let num = Number(document.getElementById('numero').value);
+    let res = document.getElementById('res');
+
+    let vetor = [];
+    vetor.push(num);
+    console.log(vetor);
+
+}
